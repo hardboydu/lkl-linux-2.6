5494b288390c0f5acfb40b52f9e5217d05573562
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-09-16 05:21:29 +0300

    lkl: atomic.h: update and use generic implementation
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- arch/lkl/include/asm/atomic.h
+++ arch/lkl/include/asm/atomic.h
@@ -1,141 +1,19 @@
 #ifndef _ASM_LKL_ATOMIC_H
 #define _ASM_LKL_ATOMIC_H
 
-#include <linux/types.h>
-#include <asm/system.h>
+#include <asm-generic/atomic.h>
 
-
-#define ATOMIC_INIT(i) { (i) }
-
-static __inline__ int atomic_read(const atomic_t *v)
-{
-	return  v->counter;
-}
-
-static __inline__ void atomic_set(atomic_t *v, int i)
-{
-	v->counter = i;
-}
-
-static __inline__ void atomic_add(int i, atomic_t *v)
-{
-	v->counter += i;
-}
-
-static __inline__ void atomic_inc(atomic_t *v)
-{
-	return atomic_add(1, v);
-}
-
-static __inline__ void atomic_sub(int i, atomic_t *v)
-{
-	return atomic_add(-i, v);
-}
-
-static __inline__ void atomic_dec(atomic_t *v)
-{
-	return atomic_add(-1, v);
-}
-
-static __inline__ int atomic_add_and_test(int i, atomic_t *v)
-{
-	int ret;
-	
-	v->counter += i;
-	ret = (v->counter == 0);
-	
-	return ret;
-}
-
-static __inline__ int atomic_sub_and_test(int i, atomic_t *v)
-{
-	return atomic_add_and_test(-i, v);
-}
-
-static __inline__ int atomic_dec_and_test(atomic_t *v)
-{
-	return atomic_add_and_test(-1, v);
-}
-
-static __inline__ int atomic_inc_and_test(atomic_t *v)
-{
-	return atomic_add_and_test(1, v);
-}
-
-static __inline__ int atomic_add_negative(int i, atomic_t *v)
-{
-	int ret;
-	
-	v->counter += i;
-	ret = (v->counter < 0);
-	
-	return ret;
-}
-
-static __inline__ int atomic_add_return(int i, atomic_t *v)
-{
-	int ret;
-	
-	v->counter += i;
-	ret = v->counter;
-	
-	return ret;
-}
-
-static __inline__ int atomic_sub_return(int i, atomic_t *v)
-{
-	return atomic_add_return(-i,v);
-}
-
-static __inline__ int atomic_inc_return(atomic_t *v)
-{
-	return atomic_add_return(1,v);
-}
-
-static __inline__ int atomic_dec_return(atomic_t *v)
-{
-	return atomic_add_return(-1,v);
-}
-
-static inline int atomic_sub_if_positive(int i, atomic_t *v)
-{
-	int ret;
-	
-	ret = v->counter - i;
-	if (v->counter >= i)
-	    v->counter -= i;
-	return ret;
-}
 
 static __inline__ int atomic_dec_if_positive(atomic_t *v)
 {
-	return atomic_sub_if_positive(1, v);
-}
-
-/**
- * atomic_add_unless - add unless the number is a given value
- * @v: pointer of type atomic_t
- * @a: the amount to add to v...
- * @u: ...unless v is equal to u.
- *
- * Atomically adds @a to @v, so long as it was not @u.
- * Returns non-zero if @v was not @u, and zero otherwise.
- */
-static __inline__ int atomic_add_unless(atomic_t *v, int a, int u)
-{
-	int ret=1;
+	int ret;
 	
-        if (v->counter != u)
-                v->counter += a;
-        else
-                ret = 0;
+	ret = v->counter - 1;
+	if (ret >= 0)
+	    v->counter = ret;
 	return ret;
 }
 
-static __inline__ int atomic_inc_not_zero(atomic_t *v)
-{
-        return atomic_add_unless(v, 1, 0);
-}
 
 #define xchg(ptr, v)	       \
   ({			       \
@@ -147,8 +25,6 @@ static __inline__ int atomic_inc_not_zero(atomic_t *v)
   })
 
 
-#define atomic_xchg(v, new) (xchg(&((v)->counter), (new)))
-#define atomic_cmpxchg(v, old, new) ((int)cmpxchg(&((v)->counter), (old), (new)))
 
 #include <asm-generic/cmpxchg-local.h>
 
@@ -165,6 +41,5 @@ static __inline__ int atomic_inc_not_zero(atomic_t *v)
 /* this include will make us fail on SMP. */
 #include <asm-generic/cmpxchg.h>
 
-#include <asm-generic/atomic.h>
 
 #endif
