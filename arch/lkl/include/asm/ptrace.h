ba84d5dbcbc9a9906d8d9b32cd491af19f2b6f70
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2010-09-18 08:07:50 +0300

    lkl: signal: use asm-generic implementation
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

--- arch/lkl/include/asm/ptrace.h
+++ arch/lkl/include/asm/ptrace.h
@@ -15,7 +15,6 @@ static inline long profile_pc(struct pt_regs *regs)
         return 0;
 }
 
-#define ptrace_signal_deliver(regs, cookie) 
 
 static inline void ptrace_disable(struct task_struct *child)
 { 
